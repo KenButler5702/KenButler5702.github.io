938f468c18b1e3253d94c99c0f03631dd051f098
KenButler5702 <ken.butler920@gmail.com>
2018-03-09 19:48:55 -0500

    First Upload

---
+++ Javascripts/application.js
@@ -0,0 +1,9 @@
+//application.js
+
+$(document).ready(function() {
+  $(".logo img").hide().fadeIn(2000);
+});
+
+$(document).ready(function() {
+  $("h2")
+});
---
+++ Javascripts/main.js
@@ -0,0 +1,16 @@
+let navigation = function(){
+
+let navigationArray = [
+  {title: 'Home', url: '"Home.html"'},
+  {title: 'Work Experience', url: '"Work Experience.html"'},
+  {title: 'Contact', url: '"Contact.html"'},
+  {title: 'Portfolio', url: '"Portfolio.html"'},
+  {title: 'Skills', url: '"Skills.html"'}
+]
+
+
+for (var i = 0, navList = '', x = navigationArray.length; i < x; i++) {
+ document.write('<a href=' + navigationArray[i].url + '>' + navigationArray[i].title + '</a>');
+}
+
+}();
